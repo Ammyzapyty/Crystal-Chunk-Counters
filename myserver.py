81a3851d420187f53dadfa0ded8164dfe3cae592
Ammyzapyty <achirayasaikhamwong@gmail.com>
2025-04-24 18:55:41 +0700

    myserver.py

--- myserver.py
+++ myserver.py
@@ -4,12 +4,12 @@ from threading import Thread
 app = Flask('')
 
 @app.route('/')
-def home() :
-    return 'Server is running!'
+def home():
+    return "Server is running!"
 
-def run() :
-    app.run(host='0.0.0.0',port=8080)
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
-def server_on() :
+def server_on():
     t = Thread(target=run)
-    t.start
+    t.start()
